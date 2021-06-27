e8a246b4a08e3ba74dda5a61ce71d44036b9590d
Hridansh <73342299+Hridansh@users.noreply.github.com>
2021-06-27 14:18:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
-function setup()
-{
-    canvas = createCanvas(300, 300);
-    canvas.center();
-    video = createCapture(VIDEO);
-    video.hide();
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/648BklK9V/model.json', modelLoaded);
-}
+nose_x = 0;
+nose_y = 0;
 
-function draw() {
-    image(video, 0, 0, 300, 300);
-    classifier.classify(video, got_results);
+difference = 0;
+
+right_wrist_x = 0;
+left_wrist_x = 0;
+
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNET = ml5.poseNet(video, modelLoaded);
+    poseNET.on('pose', gotPoses);
 }
 
 function modelLoaded(){
-    console.log("Model Is Loaded!!!");
+    console.log("Model is Loaded!!!");
 }
 
-function got_results(error, results){
-    if (error){
-        console.error(error);
-    } else{
-        console.log(results);
-        document.getElementById("result_object_name").innerHTML = results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+function gotPoses(result){
+    if (result.length > 0)
+    {
+        console.log(result);
+        nose_x = result[0].pose.nose.x;
+        nose_y = result[0].pose.nose.y;
+        console.log("nose_x = " + nose_x + " , nose_y = " + nose_y);
+
+        left_wrist_x = result[0].pose.leftWrist.x;
+        right_wrist_x = result[0].pose.rightWrist.x;
+
+        difference = left_wrist_x - right_wrist_x;
+        difference.floor(point);
+        final_answer = difference;
+        console.log("left wrist x = " + left_wrist_x + ", right wrist x = " + right_wrist_x + ", difference = " + difference);
     }
 }
+
+function draw(){
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "Width and Height of the name will be = " + difference + "px"
+    textSize(final_answer);
+    text('Hridansh', nose_x, nose_y);
+    fill(245, 245, 245);
+}
+
+
